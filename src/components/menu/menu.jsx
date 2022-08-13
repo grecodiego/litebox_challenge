4e6dbdc6ce96274dd6fd9a7a70ba485d11bcc887
Diego Greco <greco.diego@gmail.com>
2022-08-12 22:23:21 -0300

    new doc menu options

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,33 +1,28 @@
 import { StyledMenu } from './menu.styled'
 import { OptionContainer } from './components/optionContainer/optionContainer'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { menuOptions } from './menuOptions'
 
-export const Menu = ({ menuOpen, handleMenuButton }) => {
+export const Menu = ({ isOpen, closeMenu }) => {
+	const navigate = useNavigate()
 	const [optionChoosen, setOptionChoosen] = useState({
 		id: 1,
 		title: 'INICIO',
 	})
-	const menuOptions = [
-		{ id: 1, title: 'INICIO', handle: () => {}, path: '' },
-		{ id: 2, title: 'SERIES', handle: () => {}, path: '' },
-		{ id: 3, title: 'PELICULAS', handle: () => {}, path: '' },
-		{ id: 4, title: 'AGREGADAS RECIENTEMENTE', handle: () => {}, path: '' },
-		{ id: 5, title: 'POPULARES', handle: () => {}, path: '' },
-		{ id: 6, title: 'MIS PELICULAS', handle: () => {}, path: '' },
-		{ id: 7, title: 'MI LISTA', handle: () => {}, path: '' },
-		{ id: 8, title: '+ AGREGAR PELICULA', handle: () => {}, path: '/add-film' },
-		{ id: 9, title: 'CERRAR SESION', handle: () => {}, path: '' },
-	]
 
 	function handleClickOption(option) {
 		setOptionChoosen(option)
-		handleMenuButton()
+		closeMenu()
+		navigate(option.path)
 	}
+
 	return (
-		<StyledMenu menuOpen={menuOpen}>
+		<StyledMenu isOpen={isOpen}>
 			{menuOptions.map((option) => {
 				return (
 					<OptionContainer
+						key={option.id}
 						option={option}
 						optionChoosen={optionChoosen}
 						handleClickOption={handleClickOption}
---
+++ src/components/menu/menuOptions.jsx
@@ -0,0 +1,11 @@
+export const menuOptions = [
+	{ id: 1, title: 'INICIO', handle: () => {}, path: '' },
+	{ id: 2, title: 'SERIES', handle: () => {}, path: '' },
+	{ id: 3, title: 'PELICULAS', handle: () => {}, path: '' },
+	{ id: 4, title: 'AGREGADAS RECIENTEMENTE', handle: () => {}, path: '' },
+	{ id: 5, title: 'POPULARES', handle: () => {}, path: '' },
+	{ id: 6, title: 'MIS PELICULAS', handle: () => {}, path: '' },
+	{ id: 7, title: 'MI LISTA', handle: () => {}, path: '' },
+	{ id: 8, title: '+ AGREGAR PELICULA', handle: () => {}, path: '/add-film' },
+	{ id: 9, title: 'CERRAR SESION', handle: () => {}, path: '' },
+]
