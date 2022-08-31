e1cd40a2f7e3a0703a5d3beb472ebf92a83f6082
Diego Greco <greco.diego@gmail.com>
2022-08-31 17:18:43 -0300

    add env variables and change to random movies

--- src/repository/mainFilms/mainFilmsRepo.js
+++ src/repository/mainFilms/mainFilmsRepo.js
@@ -1,16 +1,16 @@
 import { fetchFilmsData } from '../mainFilms/mainFilmsAPI'
 
 // Use this in case want a film aleatory at first page popularity filter
-/* function getRandomInt(minN, maxN) {
+function getRandomInt(minN, maxN) {
 	let min = Math.ceil(minN)
 	let max = Math.floor(maxN)
 	return Math.floor(Math.random() * (max - min) + min)
-} */
+}
 
-export async function getFilmData () {
-  const aleatoryFilmsArray = await fetchFilmsData()
-  const aleatoryFilm = aleatoryFilmsArray[0] // if want the most popular film, just delete 'getRandomInt(0, 7)' and put '0'
-  return aleatoryFilm
+export async function getFilmData() {
+	const aleatoryFilmsArray = await fetchFilmsData()
+	const aleatoryFilm = aleatoryFilmsArray[getRandomInt(0, 7)] // if want the most popular film, just delete 'getRandomInt(0, 7)' and put '0'
+	return aleatoryFilm
 }
 
 // I think is more fun the app with aleatory background films..
